78c8bdd5d95c05ef84da13db601f2c4055621181
Boning Li <44139981+BBBonnie@users.noreply.github.com>
2022-04-03 02:53:10 -0700

    Update app.py

--- app.py
+++ app.py
@@ -20,13 +20,22 @@ class Recipe:
 def index():
     return render_template("result/home.html")
 
+@app.route("/inventory", methods=["GET", "POST"])
+def inv():
+    return render_template("inventory.html")
+
+
 
 @app.route('/results', methods=["GET", "POST"])
 def results():  # put application's code here
     inventory = ['bourbon', 'Cointreau', 'lime juice', 'sweetener', 'ice', 'lime wedge']
-    posts = []
+    posts0 = []
+    posts1 = []
+    posts2 = []
+    posts3 = []
     with open('gimmesomeovenRecipes.json', encoding='utf-8') as rawdata:
         data = json.load(rawdata)
+    count = 0
     for d in data:
         # print(d)
 
@@ -44,9 +53,23 @@ def results():  # put application's code here
         # print(len(recipe.ingredients))
         # print(score)
         # break
-        posts.append(d)
+        if count == 0:
+            posts0.append(d)
+            count +=1
+        elif count == 1:
+            posts1.append(d)
+            count +=1
+        elif count == 2:
+            posts2.append(d)
+            count +=1
+        elif count == 3:
+            posts3.append(d)
+            count =0
+    print(len(posts0))
 
-    return render_template("result/results.html", posts=posts)
+
+
+    return render_template("result/results.html", posts0=posts0,posts1=posts1,posts2=posts2,posts3=posts3)
 
 
 if __name__ == '__main__':
